03e5e6d7e71a4c67b2ede7fe7c0b2df9dfe91225
jlucasoliveirat <jose.lucas@teclat.com.br>
2024-01-30 13:59:45 -0300

    feat: add user guard

---
+++ src/auth/user.decorator.ts
@@ -0,0 +1,9 @@
+import { Request } from 'express';
+import { createParamDecorator, ExecutionContext } from '@nestjs/common';
+
+export const User = createParamDecorator(
+  (data: unknown, ctx: ExecutionContext) => {
+    const request = ctx.switchToHttp().getRequest() as Request;
+    return request.user;
+  },
+);

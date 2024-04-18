6041a96c6d82263d111e86042d2dbecf83def8f2
jlucasoliveira <lucasosilva68@gmail.com>
2024-04-18 11:28:04 -0300

    feat(users-services): exclude password from users

--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,11 +1,12 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import * as bcrypt from 'bcrypt';
-import { PrismaService } from 'src/prima.service';
+import { User } from '@prisma/client';
+import { PageOptions } from '@/shared/pagination/filters';
+import { PageMetaDto } from '@/shared/pagination/pageMeta.dto';
+import { PrismaService } from '@/prima.service';
+import { exclude } from '@/utils/exclude';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
-import { PageOptions } from '@/shared/pagination/filters';
-import { User } from '@prisma/client';
-import { PageMetaDto } from '@/shared/pagination/pageMeta.dto';
 
 @Injectable()
 export class UsersService {
@@ -16,17 +17,30 @@ export class UsersService {
 
     const hashedPassword = await bcrypt.hash(password, 10);
 
-    return this.prismaService.user.create({
+    const user = await this.prismaService.user.create({
       data: {
         ...data,
         password: hashedPassword,
       },
     });
+
+    return exclude(user, ['password']);
   }
 
   async findAll(props: PageOptions<User>) {
+    const select: Partial<Record<keyof User, boolean>> = {
+      id: true,
+      name: true,
+      username: true,
+      isAdmin: true,
+      createdAt: true,
+      updatedAt: true,
+      lastLogin: true,
+      phoneNumber: true,
+    };
+
     const [data, total] = await this.prismaService.$transaction([
-      this.prismaService.user.findMany(props),
+      this.prismaService.user.findMany({ ...props, select }),
       this.prismaService.user.count(),
     ]);
 
@@ -40,31 +54,37 @@ export class UsersService {
 
     if (!user) throw new NotFoundException('Usuário não encontrado');
 
-    return user;
+    return exclude(user, ['password']);
   }
 
-  async findOneByUsername(username: string) {
+  async findOneByUsername(username: string, withPassword = false) {
     const user = await this.prismaService.user.findFirst({
       where: { username },
     });
 
     if (!user) throw new NotFoundException('Usuário não encontrado');
 
-    return user;
+    return withPassword ? user : (exclude(user, ['password']) as User);
   }
 
-  async update(id: string, updateUserDto: UpdateUserDto) {
+  async update(id: string, updateUserDto: UpdateUserDto | { lastLogin: Date }) {
     await this.findOne(id);
 
-    return this.prismaService.user.update({
+    const user = await this.prismaService.user.update({
       where: { id },
       data: updateUserDto,
     });
+
+    return exclude(user, ['password']);
   }
 
   async remove(id: string) {
     await this.findOne(id);
 
-    return await this.prismaService.user.delete({ where: { id } });
+    const user = await this.prismaService.user.delete({
+      where: { id },
+    });
+
+    return exclude(user, ['password']);
   }
 }

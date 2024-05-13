5ee9a1b9373c63ff96c0e8bd2a6cb0a225581a59
jlucasoliveira <lucasosilva68@gmail.com>
2024-05-13 08:57:21 -0300

    chore: add BaseEntity type

--- src/types/prisma/index.d.ts
+++ src/types/prisma/index.d.ts
@@ -2,3 +2,9 @@ import { PrismaClient } from '@prisma/client';
 import { ITXClientDenyList } from '@prisma/client/runtime/library';
 
 export type Transaction = Omit<PrismaClient, ITXClientDenyList>;
+
+export type BaseEntity = {
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+};

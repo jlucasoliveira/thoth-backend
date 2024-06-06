1446ba443ffb58c95a8ff51b73eb7787d058632e
jlucasoliveira <lucasosilva68@gmail.com>
2024-06-06 11:29:59 -0300

    feat(orders): add order controller

---
+++ src/orders/orders.controller.ts
@@ -0,0 +1,57 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  ParseIntPipe,
+  ParseUUIDPipe,
+  Patch,
+  Post,
+  Query,
+} from '@nestjs/common';
+import { OrdersService } from './orders.service';
+import { CreateOrderDTO } from './dto/create-order.dto';
+import { FilterPipe, SortPipe } from '@/shared/pagination/filters.pipe';
+import { Filter } from '@/shared/pagination/pageOptions.dto';
+import { OrderBy } from '@/shared/pagination/filters';
+import { Order } from '@prisma/client';
+import { UpdateOrderDTO } from './dto/update-order.dto';
+
+@Controller('orders')
+export class OrdersController {
+  constructor(private readonly ordersService: OrdersService) {}
+
+  @Post()
+  create(@Body() payload: CreateOrderDTO) {
+    return this.ordersService.create(payload);
+  }
+
+  @Get()
+  findAll(
+    @Query('filter', FilterPipe) where: Filter<Order>,
+    @Query('sort', SortPipe) orderBy: OrderBy<Order>,
+    @Query('skip', new ParseIntPipe({ optional: true })) skip: number = 0,
+    @Query('take', new ParseIntPipe({ optional: true })) take: number = 10,
+  ) {
+    return this.ordersService.findAll({ orderBy, skip, take, where });
+  }
+
+  @Get(':id')
+  findOne(@Param('id', ParseUUIDPipe) id: string) {
+    return this.ordersService.findOne(id);
+  }
+
+  @Patch(':id')
+  update(
+    @Param('id', ParseUUIDPipe) id: string,
+    @Body() payload: UpdateOrderDTO,
+  ) {
+    return this.ordersService.update(id, payload);
+  }
+
+  @Delete(':id')
+  delete(@Param('id', ParseUUIDPipe) id: string) {
+    return this.ordersService.delete(id);
+  }
+}

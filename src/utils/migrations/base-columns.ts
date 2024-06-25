86506bfca945e6d19680cc38d22443e829722e6b
jlucasoliveira <lucasosilva68@gmail.com>
2024-06-25 11:23:27 -0300

    chore: add BaseEntity columns helpers

---
+++ src/utils/migrations/base-columns.ts
@@ -0,0 +1,44 @@
+import { TableCheck, TableColumn } from 'typeorm';
+
+export function generateUUIDCheck(column: string, columnName = column) {
+  return new TableCheck({
+    columnNames: [columnName],
+    expression: `REGEXP_LIKE(${column}, '^[0-9a-f]{8}-([0-9a-f]{4}-){3}[0-9a-f]{12}$')`,
+  });
+}
+
+export const baseColumns: TableColumn[] = [
+  new TableColumn({
+    name: 'created_at',
+    type: 'TIMESTAMP',
+  }),
+  new TableColumn({
+    name: 'updated_at',
+    type: 'TIMESTAMP',
+  }),
+  new TableColumn({
+    name: 'deleted_at',
+    type: 'TIMESTAMP',
+    isNullable: true,
+  }),
+];
+
+export function generateBaseColumns(uuidAsId = true) {
+  return baseColumns.concat(
+    new TableColumn({
+      name: 'id',
+      type: uuidAsId ? 'CHAR(36)' : 'NUMBER',
+      isPrimary: true,
+      isGenerated: true,
+      generationStrategy: uuidAsId ? 'uuid' : 'increment',
+    }),
+  );
+}
+
+export function generatePhoneNumberColumn(isNullable = true) {
+  return new TableColumn({
+    isNullable,
+    name: 'phone_number',
+    type: 'VARCHAR2(20)',
+  });
+}

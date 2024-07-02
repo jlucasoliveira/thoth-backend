3de1fc2a900a3e1abca8d70cd34bae32b3426713
jlucasoliveira <lucasosilva68@gmail.com>
2024-07-02 11:00:06 -0300

    feat(filters): extract filters parser and implement typeorm AND and OR find operators

---
+++ src/shared/pagination/filter-parser.ts
@@ -0,0 +1,145 @@
+import {
+  And,
+  Between,
+  Equal,
+  ILike,
+  In,
+  LessThan,
+  LessThanOrEqual,
+  Like,
+  MoreThan,
+  MoreThanOrEqual,
+  Not,
+  Or,
+} from 'typeorm';
+import { Filter, FilterOperator } from './pageOptions.dto';
+import { WhereClause } from './filters';
+
+export class FilterParser<T> {
+  private query: WhereClause<T>;
+
+  convert(input: any): any {
+    const number = Number(input);
+    if (!isNaN(number)) return number;
+
+    const inDate = new Date(input);
+    if (inDate.toString() !== 'Invalid Date') return inDate;
+
+    if (Array.isArray(input)) return input.map((value) => this.convert(value));
+
+    return input;
+  }
+
+  constructor(private readonly filter?: Filter<T>) {
+    this.query = {};
+  }
+
+  createEquals(value: T[keyof T]) {
+    return Equal(this.convert(value));
+  }
+
+  createNotEquals(value: T[keyof T]) {
+    return Not(this.convert(value));
+  }
+
+  createContains(value: T[keyof T]) {
+    return Like(this.convert(value));
+  }
+
+  createInsensitiveContains(value: T[keyof T]) {
+    return ILike(this.convert(value));
+  }
+
+  createGreaterThanEqual(value: T[keyof T]) {
+    return MoreThanOrEqual(this.convert(value));
+  }
+
+  createLessThanEqual(value: T[keyof T]) {
+    return LessThanOrEqual(this.convert(value));
+  }
+
+  createGreaterThan(value: T[keyof T]) {
+    return MoreThan(this.convert(value));
+  }
+
+  createLessThan(value: T[keyof T]) {
+    return LessThan(this.convert(value));
+  }
+
+  createBetween([start, end]: [T[keyof T], T[keyof T]]) {
+    return Between(this.convert(start), this.convert(end));
+  }
+
+  createInterval(interval: T[keyof T] | Array<T[keyof T]>) {
+    if (!Array.isArray(interval)) interval = [interval];
+    return In(this.convert(interval));
+  }
+
+  createDisjunction(filters: Array<FilterOperator<T[keyof T]>>) {
+    return Or(
+      ...Object.entries(filters).map(([key, filter]) => {
+        const parser = this.parserFactory(
+          key as keyof FilterOperator<T[keyof T]>,
+        );
+        return parser(filter as any);
+      }),
+    );
+  }
+
+  createJunction(filters: Array<FilterOperator<T[keyof T]>>) {
+    return And(
+      ...Object.entries(filters).map(([key, filter]) => {
+        const parser = this.parserFactory(
+          key as keyof FilterOperator<T[keyof T]>,
+        );
+        return parser(filter as any);
+      }),
+    );
+  }
+
+  createNestedQuery(value: Filter<T[keyof T]>) {
+    return new FilterParser(value).generateQuery();
+  }
+
+  private parserFactory(key: keyof FilterOperator<T[keyof T]>) {
+    const factories = {
+      eq: this.createEquals.bind(this),
+      ne: this.createNotEquals.bind(this),
+      like: this.createContains.bind(this),
+      ilike: this.createInsensitiveContains.bind(this),
+      between: this.createBetween.bind(this),
+      lt: this.createLessThan.bind(this),
+      gt: this.createGreaterThan.bind(this),
+      lte: this.createLessThanEqual.bind(this),
+      gte: this.createGreaterThanEqual.bind(this),
+      in: this.createInterval.bind(this),
+      and: this.createJunction.bind(this),
+      or: this.createDisjunction.bind(this),
+    };
+
+    const parser = factories[key as keyof typeof factories];
+
+    if (!parser) {
+      // ! With a invalid filter or a unknown relation will raise too deep recursion
+      return this.createNestedQuery;
+      // throw new BadRequestException('Invalid filter');
+    }
+
+    return parser;
+  }
+
+  generateQuery(): WhereClause<T> {
+    if (this.filter) {
+      Object.entries(this.filter).forEach(([attr, filter]) => {
+        const [key, value] = Object.entries(filter)[0];
+        const parser = this.parserFactory(
+          key as keyof FilterOperator<T[keyof T]>,
+        );
+
+        this.query[attr] = parser(value as any);
+      });
+    }
+
+    return this.query;
+  }
+}
--- src/shared/pagination/filters.ts
+++ src/shared/pagination/filters.ts
@@ -1,16 +1,3 @@
-import {
-  Between,
-  ILike,
-  In,
-  LessThan,
-  LessThanOrEqual,
-  Like,
-  MoreThan,
-  MoreThanOrEqual,
-  Not,
-} from 'typeorm';
-import { Filter } from './pageOptions.dto';
-
 type QueryKey =
   | 'equals'
   | 'in'
@@ -26,7 +13,7 @@ type QueryKey =
 
 type Query<Type> = Record<QueryKey, Type>;
 
-type WhereClause<T> = {
+export type WhereClause<T> = {
   [key in keyof T]?: T[key] | Query<T[key]> | any;
 };
 
@@ -38,84 +25,3 @@ export type PageOptions<Entity> = {
   order: OrderBy<Entity>;
   where: WhereClause<Entity>;
 };
-
-function convert(input: any): any {
-  const number = Number(input);
-  if (!isNaN(number)) return number;
-
-  const inDate = new Date(input);
-  if (inDate.toString() !== 'Invalid Date') return inDate;
-
-  if (Array.isArray(input)) return input.map((value) => convert(value));
-
-  return input;
-}
-
-export function parseFilterIntoQueryWhere<EntityType>(
-  filter: Filter<EntityType>,
-): WhereClause<EntityType> {
-  const where: WhereClause<EntityType> = {};
-
-  if (!filter) return where;
-
-  Object.keys(filter).forEach((key) => {
-    const filterValue = filter[key];
-    const operator = Object.keys(filterValue)[0];
-
-    if (Array.isArray(filterValue) && !['between', 'or'].includes(operator))
-      return;
-
-    switch (operator) {
-      case 'eq':
-        where[key] = convert(filterValue['eq']);
-        break;
-      case 'ne':
-        where[key] = Not(convert(filterValue['ne']));
-        break;
-      case 'like':
-        where[key] = Like(convert(filterValue['like']));
-        break;
-      case 'ilike':
-        where[key] = ILike(convert(filterValue['ilike']));
-        break;
-      case 'between':
-        const [start, end] = filterValue['between'];
-        where[key] = Between(convert(start), convert(end));
-        break;
-      case 'in':
-        where[key] = In(convert(filterValue['in']));
-        break;
-      case 'or':
-        const orFilters = filterValue['or'];
-        where['OR'] = orFilters.map((orFilter) =>
-          parseFilterIntoQueryWhere({ [key]: orFilter }),
-        );
-        break;
-      case 'gte':
-        where[key] = MoreThanOrEqual(convert(filterValue['gte']));
-        break;
-      case 'lte':
-        where[key] = LessThanOrEqual(convert(filterValue['lte']));
-        break;
-      case 'gt':
-        where[key] = MoreThan(convert(filterValue['gt']));
-        break;
-      case 'lt':
-        where[key] = LessThan(convert(filterValue['lt']));
-        break;
-      default:
-        where[key] = parseFilterIntoQueryWhere(filterValue);
-        break;
-    }
-  });
-
-  return where;
-}
-
-export function parseSortQueryParam<SortStringType extends string>(
-  sort = '-createdAt' as SortStringType,
-): Record<SortStringType, 'asc' | 'desc'> {
-  return {
-    [sort.replace(/^-/, '')]: /^-/.test(sort) ? 'desc' : 'asc',
-  } as Record<SortStringType, 'asc' | 'desc'>;
-}

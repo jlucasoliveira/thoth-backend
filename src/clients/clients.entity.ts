233c2035860ff18d5c11f8f2db6765d8f8ad8fb2
jlucasoliveira <lucasosilva68@gmail.com>
2024-07-04 15:12:05 -0300

    feat(clients): decouple verification if client is default

--- src/clients/clients.entity.ts
+++ src/clients/clients.entity.ts
@@ -1,9 +1,12 @@
+import { AfterLoad, Column, Entity, OneToMany } from 'typeorm';
 import { OrderEntity } from '@/orders/orders.entity';
 import { BaseEntity } from '@/types/typeorm/base-entity';
-import { Column, Entity, OneToMany } from 'typeorm';
 
 @Entity({ name: 'clients' })
 export class ClientEntity extends BaseEntity {
+  isDefault = false;
+  static defaultName = 'Comprador Avulso';
+
   @Column({ length: 60 })
   name: string;
 
@@ -19,4 +22,9 @@ export class ClientEntity extends BaseEntity {
 
   @OneToMany(() => OrderEntity, (order) => order.client)
   purchases: OrderEntity[];
+
+  @AfterLoad()
+  private generateVirtualDefault() {
+    this.isDefault = this.name == ClientEntity.defaultName;
+  }
 }

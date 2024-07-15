1a5da3989d6aa666e5331f4e827b09f2a701808c
jlucasoliveira <lucasosilva68@gmail.com>
2024-07-15 14:57:45 -0300

    feat(categories): add product-category many-to-many relation

--- src/categories/categories.module.ts
+++ src/categories/categories.module.ts
@@ -1,11 +1,12 @@
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Module } from '@nestjs/common';
 import { CategoryEntity } from './categories.entity';
+import { ProductCategoryEntity } from './products-categories.entity';
 import { CategoriesService } from './categories.service';
 import { CategoriesController } from './categories.controller';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([CategoryEntity])],
+  imports: [TypeOrmModule.forFeature([CategoryEntity, ProductCategoryEntity])],
   controllers: [CategoriesController],
   providers: [CategoriesService],
   exports: [CategoriesService],
